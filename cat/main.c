ee86da62f669d76b1648dbb57c8fde65c1e93fef
dmsukhikh <dimasssuchich@mail.ru>
2025-09-15 14:50:52 +0300

    [cat]: basic cat using

--- cat/main.c
+++ cat/main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
-#include <sys/unistd.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+FILE *file = NULL;
+
+void _close_file()
+{
+    fclose(file);
+}
 
 int main(int argc, char **argv)
 {
-    printf("hi\n");
+    if (argc < 2)
+    {
+        fprintf(stderr, "[cat]: Not enough args!\n");
+        return 1;
+    }
+
+    // --- file opening ---
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "[cat]: Error during open file %s: %s\n", argv[1],
+                strerror(errno));
+        return 1;
+    }
+    atexit(_close_file);
+    // --------------------
+    
+    for (char ch; (ch = fgetc(file)) != EOF; )
+    {
+        putchar(ch);
+    }
+    return 0;
 }
