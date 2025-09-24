ed1b6f24cde92e8d3fdeaa5a51c6a0a04b8f14b6
Dmitriy Sukhikh <dimasssuchich@mail.ru>
2025-09-24 19:21:23 +0300

    [cat]: done

--- cat+grep/mycat.c
+++ cat+grep/mycat.c
@@ -3,36 +3,183 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+enum cat_flag
+{
+    FLAG_NUM = 1,
+    FLAG_NENUM = 2,
+    FLAG_END = 4
+};
+
+enum err_code
+{
+    ERR_INVALIDOPT,
+    ERR_NEARGS,
+    ERR_FILE_NOT_FOUND,
+    ERR_MEMERROR
+};
 
 FILE *file = NULL;
+uint32_t flags = 0;
+const char *OPLIST = "nbEh";
+int line_align = 0, neline_align = 0;
 
-void _close_file()
+void close_file()
 {
     fclose(file);
 }
+
+void list_routine(const char *file);
+void invoke_error(enum err_code code);
+void count_lines();
+
 
 int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        fprintf(stderr, "[cat]: Not enough args!\n");
-        return 1;
+        invoke_error(ERR_NEARGS);
     }
 
+    opterr = 0; // don't print error from getopt()
+    int option;
+    while ((option = getopt(argc, argv, OPLIST)) != -1)
+    {
+        switch (option)
+        {
+            case 'h':
+                printf("mycat - print file contents\n"
+                       "usage: my [params...] [file]\n"
+                       " -n - count every line in file \".\"\n"
+                       " -b - count every non-empty line in file\n"
+                       " -E - print delimeter in the end of lines\n---\n"
+                       "mireaaaa\n");
+                exit(EXIT_SUCCESS);
+                break;
+            case 'n':
+                flags |= FLAG_NUM;
+                break;
+            case 'b':
+                flags |= FLAG_NENUM;
+                break;
+            case 'E':
+                flags |= FLAG_END;
+                break;
+            default: 
+                invoke_error(ERR_INVALIDOPT);
+                break;
+        }
+    }
+
+    list_routine(argv[optind]);
+    // -------
+    exit(EXIT_SUCCESS);
+
+}
+
+void list_routine(const char *f)
+{
     // --- file opening ---
-    file = fopen(argv[1], "r");
+    file = fopen(f, "r");
     if (file == NULL)
     {
-        fprintf(stderr, "[cat]: Error during open file %s: %s\n", argv[1],
-                strerror(errno));
-        return 1;
+        invoke_error(ERR_FILE_NOT_FOUND);
     }
-    atexit(_close_file);
+    atexit(close_file);
     // --------------------
     
-    for (char ch; (ch = fgetc(file)) != EOF; )
+    ssize_t bytes = 0;
+    size_t sz = 0;
+
+    char* line = NULL;
+
+    errno = 0;
+    size_t lines = 1, nelines = 1;
+    
+    count_lines();
+
+    while ((bytes = getline(&line, &sz, file)) != -1)
     {
-        putchar(ch);
+        if (flags & FLAG_NENUM)
+        {
+            if (bytes > 1)
+            {
+                printf("   %*zu\t", neline_align, nelines++);
+            }
+        }
+        else if (flags & FLAG_NUM)
+        {
+            printf("  %*zu\t", line_align, lines++);
+        }
+        printf("%.*s", (int)bytes - 1, line);
+
+        if (flags & FLAG_END)
+        {
+            putchar('$');
+        }
+        putchar('\n');
     }
-    return 0;
+    
+    if (errno)
+    {
+        invoke_error(ERR_MEMERROR);
+    }
+    free(line);
+
+}
+
+void invoke_error(enum err_code err)
+{
+    switch (err)
+    {
+    case ERR_NEARGS:
+        fprintf(stderr, "[ls]: Not enough args! Usage: mycat -h\n");
+        break;
+    
+    case ERR_INVALIDOPT:
+        fprintf(stderr, "[ls]: Error: invalid option, see \"mycat -h\"\n");
+        break;
+
+    case ERR_FILE_NOT_FOUND:
+        fprintf(stderr, "[ls]: Error: file not found, see \"mycat -h\"\n");
+        break;
+
+    case ERR_MEMERROR:
+        fprintf(stderr, "[ls]: Error: allocation failed!\n");
+        break;
+    }
+
+    exit(EXIT_FAILURE);
+}
+
+void count_lines()
+{
+    size_t bytes = 0, sz = 0; 
+    char *line;
+    size_t l = 0, nel = 0;
+    while ((bytes = getline(&line, &sz, file)) != -1)
+    {
+        l++;
+        if (bytes > 1) nel++; 
+    }
+
+    while (l > 0)
+    {
+        line_align++;
+        l /= 10;
+    }
+
+    while (nel > 0)
+    {
+        neline_align++;
+        nel /= 10;
+    }
+
+    if (errno)
+    {
+        invoke_error(ERR_MEMERROR);
+    }
+    rewind(file);
+    free(line);
 }
